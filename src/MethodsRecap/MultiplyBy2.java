0c68e33fe4e60038f6653f407864123828322986
nikolaM500 <nikola.markovic2014@gmail.com>
2020-08-18 21:50:32 -0700

    MultiplyOddBy

---
+++ src/MethodsRecap/MultiplyBy2.java
@@ -0,0 +1,24 @@
+package MethodsRecap;
+//write a program that can multiply each odd number by 2
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MultiplyBy2 {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(1,2,3,4,5));
+
+        for (int i = 0; i <list.size() ; i++) {
+            if (list.get(i)%2 !=0) {
+                list.set(i,list.get(i)*2);
+            }
+        }
+        System.out.println(list.toString());
+
+
+    }
+}
